3de2865a5ee44fe4a725056cd9185895146ad293
wooktori <jjun9503@gmail.com>
2025-07-08 17:33:15 +0900

    Feat: 경로 분기에 따른 Header 컴포넌트 구분

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,29 @@
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
 export default function Header() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isMain = location.pathname === "/";
   return (
     <>
       <div className="flex items-center justify-between">
         <img
           src="https://i.namu.wiki/i/0USV9nUU5fEhlvT5ViKqwQe-uy46DVnu1YiSZYnKwgkt_8tVaRewInwz7A_CoRrsgCxfwTlBqbLo_z_tb3xZ6gmheKDSlY3jdbwhJzKSvsZcQY9D27mIRjAvMdkHeGsLYvXVTYHsNsnxey5ybmxDbQ.svg"
           width="64"
+          onClick={() => navigate("/")}
         />
-        <button className="cursor-pointer text-brand hover:text-brand-dark">
+        {isMain ? null : (
+          <nav className="flex gap-3 font-bold">
+            <Link to="">중고거래</Link>
+            <Link to="">부동산</Link>
+            <Link to="">중고차</Link>
+            <Link to="">알바</Link>
+            <Link to="">동네업체</Link>
+            <Link to="">동네생활</Link>
+            <Link to="">모임</Link>
+          </nav>
+        )}
+        <button className="cursor-pointer p-2 rounded-sm text-sm text-white bg-brand hover:bg-brand-hover">
           앱 다운로드
         </button>
       </div>
--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import App from "./App";
+import Search from "./pages/Search";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "/", element: <Home />, children: [] }],
+    children: [
+      { path: "/", element: <Home />, children: [] },
+      { path: "/search", element: <Search />, children: [] },
+    ],
   },
 ]);
 
