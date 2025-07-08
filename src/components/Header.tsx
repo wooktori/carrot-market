a45572183181dbe767a04e2740131fde652cb106
wooktori <jjun9503@gmail.com>
2025-07-08 15:01:44 +0900

    Feat: tailwind theme 설정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
           src="https://i.namu.wiki/i/0USV9nUU5fEhlvT5ViKqwQe-uy46DVnu1YiSZYnKwgkt_8tVaRewInwz7A_CoRrsgCxfwTlBqbLo_z_tb3xZ6gmheKDSlY3jdbwhJzKSvsZcQY9D27mIRjAvMdkHeGsLYvXVTYHsNsnxey5ybmxDbQ.svg"
           width="64"
         />
-        <button className="cursor-pointer hover:text-orange-400">
+        <button className="cursor-pointer text-brand hover:text-brand-dark">
           앱 다운로드
         </button>
       </div>
